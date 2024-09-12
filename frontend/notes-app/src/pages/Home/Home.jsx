ac38adb324047df45970b52b9eb2f0124546c538
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-09-12 23:40:42 +0200

    feat: Add additional props to NoteCard component for enhanced functionality
    - Introduced `tags`, `isPinned`, `onEdit`, `onDelete`, and `onPinNote` props to `NoteCard` component.
    - Integrated `MdOutlinePushPin` icon to visually represent the pinned state of a note.
    - Added a static `NoteCard` instance in the `Home` component with dummy data for title, content, date, and functionality placeholders for edit, delete, and pin.
    - Applied initial styling for note title, date, and content.

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
 
 const Home = () => {
   return (
     <>
       <Navbar/>
-      
+
+      <div className='container mx-auto'>
+        <NoteCard
+          title="Meeting Notes"
+          content="Discuss project milestones and next steps for Q4."
+          date="Sep 12, 2024"
+          tags="#Meeting"
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+
+        />
+      </div>
+
     </>
   )
 }

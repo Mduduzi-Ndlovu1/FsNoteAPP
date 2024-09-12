4a8b63d980189f2e4eac6b7ccaae541a3e0bc58c
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-09-12 19:57:23 +0200

    feat: Add ProfileInfo, Navbar, and SearchBox components for user profile and search functionality
    - Implemented `ProfileInfo` component to display user initials and a logout button.
    - Added `Navbar` component with app title, search functionality, and profile information.
    - Integrated `SearchBox` component for querying notes with a clear option and search icon.
    - Added `getInitials` utility function to generate initials from a user's name.
    - Setup routing to navigate to the login page upon logout.
    - Applied Tailwind CSS for consistent styling across components.

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom'
+import SearchBox from '../SearchBar/SearchBox'
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    navigate('/login')
+  };
+
+  const handleSearch = () => {
+    
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery('')
+  }
+
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         <h2 className='text-xl font-medium text-black py-2'>
             Notes Manager
         </h2>
+
+        <SearchBox 
+          value={searchQuery}
+          onChange={({target}) => {
+            setSearchQuery(target.value)
+          }}  
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+          
+        />
+
+        <ProfileInfo
+          onLogout={onLogout}
+        />
     </div>
   )
 }

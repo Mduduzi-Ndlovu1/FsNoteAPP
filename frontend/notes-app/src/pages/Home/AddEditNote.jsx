09c9088d9cd29b4cfdb3f5f07204b5878b3f6173
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-09-13 13:05:31 +0200

    feat: Add modal for note creation, login, and signup functionality
    - Integrated `react-modal` for adding and editing notes with the `AddEditNote` component.
    - Updated `NoteCard` component to support pinning, editing, and deleting notes.
    - Added `PasswordInput` component with show/hide password functionality.
    - Created `Login` and `SignUp` components with form validation and error handling.
    - Applied custom Tailwind CSS classes for input fields, buttons, and labels.
    - Enhanced UI with grid layout for notes and floating action button for adding notes.

---
+++ frontend/notes-app/src/pages/Home/AddEditNote.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import TagInput from '../../components/Input/TagInput'
+
+const AddEditNote = () => {
+
+    
+
+
+  return (
+    <div>
+        <div className='flex flex-col gap-2'>
+            <label className='input-label'>Title</label>
+            <input 
+                type='text'
+                className='text-2xl text-slate-950 outline-none'
+                placeholder='Go to the Gym at 5'
+            />
+        </div>
+
+        <div className='flex flex-col  gap-2 mt-4'>
+            <label className='input-label'>CONTENT</label>
+            <textarea
+                type='text'
+                className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
+                placeholder='Content'
+                rows={10}
+            />
+        </div>
+
+        <div className='mt-3'>
+            <label className='input-label'>TAGS</label>
+            <TagInput/>
+        </div>
+
+        <button 
+            className='btn-primary font-medium mt-5 p-3'
+            onClick={() => {}}
+        >
+            ADD
+        </button>
+
+        
+    </div>
+  )
+}
+
+export default AddEditNote
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../components/Cards/NoteCard'
 import { MdAdd } from 'react-icons/md'
-
+import AddEditNote from './AddEditNote'
+import Modal from "react-modal"
 const Home = () => {
+
+  const [openAddEditModel, setOpenAddEditModel] = useState({
+    isShown: false,
+    type: 'add',
+    data: null,
+});
+
   return (
     <>
       <Navbar/>
@@ -25,14 +33,30 @@ const Home = () => {
       </div>
       <button 
         className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'
-        onClick={() => {}}
+        onClick={() => {
+          setOpenAddEditModel({isShown: true, type: 'add', data: null})
+        }}
       >
         <MdAdd 
           className='text-[32px] text-white'
         />
       </button>
+      <Modal
+        isOpen={openAddEditModel.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.2)'
+          },
+        }} 
+        contentLabel = ""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-hidden"
+      >
+        <AddEditNote/>
 
+      </Modal>
       
+
     </>
   )
 }

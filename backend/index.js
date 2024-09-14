be0340ed35082f04162828f14c3ad67fa132417c
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-09-14 02:03:32 +0200

    feat(backend): Set up backend structure with user model, JWT authentication, and CORS support
    - Created `index.js` to initialize Express server with CORS configuration and JWT support
    - Added a basic JWT authentication middleware (`authenticateToken`) in `utils.js`
    - Defined `User` model with Mongoose schema in `user.model.js`
    - Set up connection to MongoDB using Mongoose with configuration from `config.json`

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utils");
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: '*',
+    })
+);
+
+app.get("/", (req, res) => {
+    res.json({data: "Hello World"});
+})
+
+//
+
+
+app.listen(8000);
+
+module.exports = app;

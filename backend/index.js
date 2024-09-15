2fc7a0b3a29eaeaf7341d0198916b0d04325fe65
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-09-15 02:06:46 +0200

    feat(backend): Implement user authentication and account creation endpoints
    - Added `config.json` for MongoDB connection string.
    - Set up MongoDB connection using Mongoose in `index.js`.
    - Created `/create-account` route for user registration, validating input and saving users to the database.
    - Created `/login` route for user login, returning JWT access token upon successful authentication.
    - Integrated `authenticateToken` middleware to secure routes with JWT authentication.
    - Updated `user.model.js` to define the `User` schema with Mongoose.

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
 
+const User = require("./models/user.model");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -24,8 +26,81 @@ app.get("/", (req, res) => {
     res.json({data: "Hello World"});
 })
 
-//
+// Create Account
+app.post("/create-account", async (req, res) => {
+    const { fullName, email, password } = req.body;
+    if (!fullName) {
+        return res.status(400).json({ error:true, message: "Please enter your full name" });
+    }
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Please enter a valid email address" });
+    }
 
+    if(!password) {
+        return res.status(400).json({ error: true, message: "Please enter password" });
+    }
+
+    if (password.length < 6) {
+        return res.status(400).json({ error: true, message: "Password must be at least 6 characters" });
+    }
+
+    const isUser = await User.findOne({ email });
+
+    if (isUser) {
+        return res.status(400).json({ error: true, message: "User already exists" });
+    }
+
+    const user = new User({
+        fullName,
+        email,
+        password
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "3600m"
+    });
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: "Registration Succesful"
+    })
+});
+
+// login into Account
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Please enter a valid email address" });
+    }
+
+    if (!password) {
+        return res.status(400).json({ error: true, message: "Please enter password" });
+    }
+
+    const userInfo = await User.findOne({ email });
+    if (!userInfo) {
+        return res.status(400).json({ error: true, message: "User does not exist" });
+    }
+
+    if (userInfo.email == email && userInfo.password == password) {
+        const accessToken = jwt.sign({ userInfo }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "3600m"
+        });
+        return res.json({
+            error: false,
+            userInfo,
+            accessToken,
+            message: "Login Successful"
+        })
+    } else {
+        return res.status(400).json({ error: true, message: "Invalid Credentials" });
+    }
+});
 
 app.listen(8000);
 
